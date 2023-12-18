3c38813b859a6693ec9d58ebb6421e29e431f3c3
Alexey Elyakin <ieliakin98@mail.ru>
2023-12-18 18:55:20 +0300

    ДЗ_4

--- src/video.py
+++ src/video.py
@@ -1,19 +1,15 @@
 import os
 from googleapiclient.discovery import build
 
-from helper.youtube_api_manual import youtube, playlist_id
-
 
 class Video:
+    YT_API_KEY: str = os.getenv('YT_API_KEY')
 
     def __init__(self, video_id):
         self.video_id = video_id
-
-        YT_API_KEY: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=video_id
+                                                          ).execute()
 
         self.title = video_response["items"][0]["snippet"]["title"]
         self.url = f'https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A{self.video_id}'
@@ -23,13 +19,17 @@ class Video:
     def __str__(self):
         return f'{self.title}'
 
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=cls.YT_API_KEY)
+        return youtube
+
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-    playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-                                                   part='contentDetails',
-                                                   maxResults=50,
-                                                   ).execute()
+        self.playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id,
+                                                                       part='contentDetails',
+                                                                       maxResults=50,
+                                                                       ).execute()
